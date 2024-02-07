68d37bba3b5511036139833ef33ce63fd4e32095
Sohel <sohailmaniyar99@gmail.com>
2024-02-07 23:52:45 +0530

    feb720204commit

--- src/main/java/com/smart/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smart/smartcontactmanager/controller/UserController.java
@@ -5,9 +5,12 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.smartcontactmanager.dao.UserRepository;
+import com.smart.smartcontactmanager.entities.Contact;
 import com.smart.smartcontactmanager.entities.User;
 @Controller
 @RequestMapping("/user")
@@ -15,15 +18,34 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/index")
-    public String dashboard(Model model,Principal principal){
+    //method to adding commomn data to response
+    @ModelAttribute
+    public void addCommonData(Model model,Principal principal){
         String userName=principal.getName();
         System.out.println("USERNAME"+userName);
 
         User user=userRepository.getUserByUserName(userName);
         System.out.println("USER "+user);
         model.addAttribute("user", user);
+          
+    }
+
+    @RequestMapping("/index")
+    public String dashboard(Model model,Principal principal){
+        // String userName=principal.getName();
+        // System.out.println("USERNAME"+userName);
+
+        // User user=userRepository.getUserByUserName(userName);
+        // System.out.println("USER "+user);
+        // model.addAttribute("user", user);
+        // 
         return "normal/user_dashboard";
+    }
+    @GetMapping("/add_contact")
+    public String openAddContactForm(Model model){
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
+        return "normal/add_contact";
     }
 
     
