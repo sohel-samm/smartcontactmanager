5f9049d35bdcc6264cf4201dfb5c546d6355bd13
Sohel <sohailmaniyar99@gmail.com>
2024-02-13 12:50:00 +0530

    through OTP password change module completed,all error-success msgs shown correctly and its 90 % perfect

--- src/main/java/com/smart/smartcontactmanager/controller/ForgotController.java
+++ src/main/java/com/smart/smartcontactmanager/controller/ForgotController.java
@@ -3,7 +3,9 @@ package com.smart.smartcontactmanager.controller;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +23,8 @@ public class ForgotController {
     private MailService mailService;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bcrypt;
     
     @RequestMapping("/forgot")
     public String openEmailForm(){
@@ -29,7 +33,7 @@ public class ForgotController {
     }
 
     @PostMapping("/send-otp")
-    public String sendOTP(@RequestParam("email") String email,HttpSession session){
+    public String sendOTP(@RequestParam("email") String email,HttpSession session,Model model){
        System.out.println("EMAIL "+email);
        Random random= new Random(1000);
        int otp=random.nextInt(9999);
@@ -44,13 +48,14 @@ public class ForgotController {
 
        } catch (Exception e) {
         e.printStackTrace();
+        model.addAttribute("message", "OTP sent failed!! Try again");
         return "forgot_email_form";
        }
        
 
     }
     @PostMapping("/verify-otp")
-    public String verifyOtp(@RequestParam("otp") int otp,HttpSession session){
+    public String verifyOtp(@RequestParam("otp") int otp,HttpSession session,Model model){
         int myotp=(int)session.getAttribute("otp");
         String email=(String)session.getAttribute("email");
 
@@ -61,6 +66,8 @@ public class ForgotController {
           if(user==null){
             //send error message
             //user doesnt exist
+            model.addAttribute("message", "User Doesnt exist!!!");
+
             return "forgot_email_form";
           }
           else{
@@ -69,9 +76,23 @@ public class ForgotController {
             return "password_change_form";
         }
         else{
+            model.addAttribute("message", "OTP Incorrect!!!");
+
             return "forgot_email_form";
         }
 
+
+    }
+
+    @PostMapping("/change-password")
+    public String changePassword(@RequestParam("newpassword") String newpassword,HttpSession session,Model model){
+
+        String email=(String)session.getAttribute("email");
+        User user=this.userRepository.getUserByUserName(email);
+        user.setPassword(this.bcrypt.encode(newpassword));
+        this.userRepository.save(user);
+        model.addAttribute("message", "Password changed succesfully");
+        return "login";
 
     }
 }
